e24420dd35184b064ffccc6ae6c52ed01b3afc5d
Vivek Kandathil <32603391+vivekandathil@users.noreply.github.com>
2020-07-07 11:21:11 -0400

    Added search bar, haptic feedback, card progress

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Image,
   SafeAreaView,
   YellowBox,
+  Button,
 } from "react-native";
 import Swiper from "react-native-deck-swiper";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -15,21 +16,70 @@ import Dialog, { DialogContent } from "react-native-popup-dialog";
 import NumericInput from "react-native-numeric-input";
 import * as Haptics from "expo-haptics";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import { SearchBar, Overlay } from "react-native-elements";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
 
 const data = [
   {
     id: "713733696142",
     price: 1.99,
     name: "Apple",
+    information: "Pink Lady or Honeycrisp",
     image:
       "https://oppy.com/sites/default/files/styles/large/public/Pink-Lady_hero.png?itok=S69rflgO",
+    category: "Fruit-Vegetable",
+    quantity: 1,
+  },
+  {
+    id: "021248000072",
+    price: 1.45,
+    name: "Banana",
+    information: "",
+    image:
+      "http://2.bp.blogspot.com/-pU8BDUCR3Sg/UDZQmt4IcdI/AAAAAAAAACA/T5rq7xIKLek/s1600/Banana.png",
+    category: "Fruit-Vegetable",
+    quantity: 1,
+  },
+  {
+    id: "715756100040",
+    price: 3.99,
+    name: "Raspberries",
+    information: "",
+    image:
+      "https://www.driscolls.pt/assets/styles/block_product_display/public/content/block/image/isa_2754_frambozen-125gr-zijkant-lowres.png?itok=W5LBTWau",
+    category: "Fruit-Vegetable",
+    quantity: 1,
+  },
+  {
+    id: "715756300129",
+    price: 3.99,
+    name: "Blueberries",
+    information: "",
+    image:
+      "https://0bb8856ba8259ec33e3d-a40599a114f3a4c6d0979c3ffe0b2bf5.ssl.cf2.rackcdn.com/0715756300020_CL_hyvee_default_large.png",
+    category: "Fruit-Vegetable",
+    quantity: 1,
+  },
+  {
+    id: "063100231545",
+    price: 3.69,
+    name: "Canned Chicken",
+    information: "Maple Leaf Flakes of Chicken",
+    image:
+      "https://az836796.vo.msecnd.net/media/image/product/en/medium/0006310023154.jpg",
+    category: "Meat",
+    quantity: 1,
   },
   {
     id: "025293001220",
     price: 3.99,
     name: "Silk Coconut Milk",
+    information: "In the Allergy-Free section",
     image:
       "https://e22d0640933e3c7f8c86-34aee0c49088be50e3ac6555f6c963fb.ssl.cf2.rackcdn.com/0025293002280_CL_default_default_large.jpeg",
+    category: "Dairy-Egg",
+    quantity: 1,
   },
 ];
 
@@ -82,54 +132,109 @@ const CardDetails = ({ index }) => (
 
 const swiperRef = React.createRef();
 const transitionRef = React.createRef();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  const [index, setIndex] = React.useState(0);
-  const [addPopupVisible, setVisible] = React.useState(false);
+  const [index, setIndex] = React.useState(0); // Card index
+  const [addPopupVisible, setVisible] = React.useState(false); // Incrementor popup
+  const [endOfList, setEndPopup] = React.useState(false); // End of list popup
+  const [search, setSearch] = React.useState(""); // Search
+  const [searchResultsVisible, setOverlayVisible] = React.useState(false); // Search
+
+  const filteredData = data.filter((item) => {
+    return item.name.includes(search);
+  });
 
   const onSwiped = () => {
     transitionRef.current.animateNextTransition();
     setIndex((index + 1) % data.length);
+    // Check if the list is done
+    setEndPopup(index + 1 == data.length);
   };
   const onSwipedRight = () => {
     selectedItems.push(data[index]);
-    console.log(data[index]);
+    console.log(selectedItems);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
   };
   const onTapCard = () => {
     setVisible(true);
-    Haptics.selectionAsync();
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
     console.log(data[index]);
+  };
+  const updateSearch = (search) => {
+    //overlay visible
+    setOverlayVisible(true);
+    setSearch({ search });
+  };
+  const clearOverlay = (search) => {
+    //overlay invisible
+    console.log("trda");
+    setOverlayVisible(false);
   };
 
   return (
     <View style={styles.container}>
       <StatusBar hidden />
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Text>hello</Text>
+        </Drawer.Navigator>
+      </NavigationContainer>
+      <SearchBar
+        placeholder="Search for a Product..."
+        onChangeText={updateSearch}
+        value={search}
+        lightTheme
+        containerStyle={{ backgroundColor: "white" }}
+        inputContainerStyle={{ backgroundColor: "#f2f2f2" }}
+        onClear={clearOverlay}
+      />
       <Dialog
         visible={addPopupVisible}
         onTouchOutside={() => {
           setVisible(false);
         }}
       >
-        <DialogContent>
-          <NumericInput
-            onChange={(value) => console.log(value)}
-            totalWidth={240}
-            totalHeight={50}
-            iconSize={25}
-            step={1}
-            valueType="real"
-            rounded
-            textColor="#000"
-            iconStyle={{ color: "white" }}
-            rightButtonBackgroundColor={colors.green}
-            leftButtonBackgroundColor={colors.red}
-            maxValue={10}
-            minValue={1}
-            style={{ paddingTop: 40 }}
+        <DialogContent style={styles.popup}>
+          <View style={{ marginTop: 20, marginBottom: 20 }}>
+            <Text>{data[index].information}</Text>
+            <NumericInput
+              onChange={(value) => console.log(value)}
+              totalWidth={240}
+              totalHeight={50}
+              iconSize={25}
+              step={1}
+              valueType="real"
+              rounded
+              textColor="#000"
+              iconStyle={{ color: "white" }}
+              rightButtonBackgroundColor={colors.green}
+              leftButtonBackgroundColor={colors.red}
+              maxValue={10}
+              minValue={1}
+              initValue={1}
+            />
+          </View>
+        </DialogContent>
+      </Dialog>
+      <Dialog visible={endOfList}>
+        <DialogContent style={styles.popup}>
+          <Text>You have reached the end of the stack</Text>
+          <Button title="Review/Export Grocery List" />
+          <Button
+            title="Return to Stack"
+            onPress={() => {
+              setEndPopup(false);
+            }}
           />
         </DialogContent>
       </Dialog>
+
+      <View style={styles.topContainer}></View>
       <View style={styles.swiperContainer}>
+        <Text style={styles.outof}>
+          {index + 1}/{data.length}
+        </Text>
         <Swiper
           ref={swiperRef}
           cards={data}
@@ -220,11 +325,17 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
   swiperContainer: {
-    flex: 0.55,
+    flex: 0.58,
   },
   bottomContainer: {
     flex: 0.45,
     justifyContent: "space-evenly",
+  },
+  topContainer: {
+    flex: 0.03,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
   },
   bottomContainerMeta: { alignContent: "flex-end", alignItems: "center" },
   bottomContainerButtons: {
@@ -258,10 +369,20 @@ const styles = StyleSheet.create({
     color: colors.white,
     backgroundColor: "transparent",
   },
-  text: { fontFamily: "Avenir-Light", fontSize: 22 },
+  text: { fontFamily: "Avenir-Light", fontSize: 33 },
   heading: { fontSize: 24, marginBottom: 10, color: colors.gray },
   price: { color: colors.green, fontSize: 32, fontWeight: "500" },
+  outof: {
+    textAlign: "center",
+    fontFamily: "Avenir-Light",
+    fontSize: 26,
+    color: "#dc143c",
+  },
   cardDetails: {
     alignItems: "center",
+  },
+  popup: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
